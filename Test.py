12be9b098b32a00655e698b7ac8256daeb63a83d
ao00 <ao00@users.noreply.github.com>
2015-03-22 22:44:19 +0700

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,38 @@
+# Conversion Functions
+
+from getCSVValueFromIndex import *
+from getCSVStrFromIndex import *
+from getDirection import *
+from getDistance import *
+
+
+#Master Test Function
+def masterTestFunction():
+
+    if not testGetCSVStrFromIndex():
+        return False
+
+    if not testGetCSVFloatFromIndex():
+        return False
+
+    if not testGetFireValueFunctions():
+        return False
+
+    if not testGetWeatherValueFunctions():
+        return False
+
+    if not TestgetDirection():
+        return False
+
+    if not TestgetDistance():
+        return False
+
+    return True
+
+
+#testMode = False
+testMode = True
+if testMode:
+    if not masterTestFunction():
+        print "Regression Test Failed"
+        
